3b509467ba9bd3368d402a2bebedaa1a7e1861b1
UrielOfir <urielofir@gmail.com>
2021-06-04 18:48:10 +0300

    sound for every word

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -2,14 +2,38 @@
 
   <q-page class="flex flex-center">
     <div>
+      <audio id="asur">
+        <source src="../assets/audio/asur.mp3" type="audio/mpeg">
+      </audio>
+      <audio id="barur">
+        <source src="../assets/audio/barur.mp3" type="audio/mpeg">
+      </audio>
+      <audio id="gever">
+        <source src="../assets/audio/gever.mp3" type="audio/mpeg">
+      </audio>
+      <audio id="hayom">
+        <source src="../assets/audio/hayom.mp3" type="audio/mpeg">
+      </audio>
+      <audio id="isha">
+        <source src="../assets/audio/isha.mp3" type="audio/mpeg">
+      </audio>
       <audio id="metzuian">
         <source src="../assets/audio/metzuian.mp3" type="audio/mpeg">
       </audio>
+      <audio id="mi">
+        <source src="../assets/audio/mi.mp3" type="audio/mpeg">
+      </audio>
+      <audio id="mitztaer">
+        <source src="../assets/audio/mitztaer.mp3" type="audio/mpeg">
+      </audio>
+      <audio id="rega">
+        <source src="../assets/audio/rega.mp3" type="audio/mpeg">
+      </audio>
+      <audio id="shalom">
+        <source src="../assets/audio/shalom.mp3" type="audio/mpeg">
+      </audio>
     </div>
     <div class="row">
-<!--      <audio id="try">-->
-<!--        <source src="../assets/audio/2021-05-25-20:24:31.mp3" type="audio/mpeg">-->
-<!--      </audio>-->
       <h4> Sign language identifier </h4>
     </div>
     <div class="break"></div>
@@ -47,9 +71,9 @@
 
       <q-btn type="button" rounded class="btn" @click="init()">Start</q-btn>
 
-      <q-btn type="button" rounded class="btn" @click="stop()">Stop</q-btn>
+      <q-btn type="button" rounded class="btn" v-bind:class="{active: active=='stop'}" @click="stop()">Stop</q-btn>
 
-      <q-btn type="button" rounded class="btn" @click="webcam.play(); predictStop=false">play</q-btn>
+      <q-btn type="button" rounded class="btn" v-bind:class="{active: active=='play'}" @click="play()">play</q-btn>
 
     </div>
     <div class="break"></div>
@@ -58,16 +82,16 @@
         <q-card-section id="webcam-container"></q-card-section>
         <div v-if="prediction">
           <div class="font_size">
-          <q-card-section>
-            {{ prediction.className }}
-          </q-card-section>
+            <q-card-section>
+              {{ prediction.className }}
+            </q-card-section>
           </div>
         </div>
         <div v-else>
           <div class="font_size2">
-          <q-card-section>
-            לא זוהתה מילה
-          </q-card-section>
+            <q-card-section>
+              לא זוהתה מילה
+            </q-card-section>
           </div>
         </div>
       </q-card>
@@ -97,6 +121,7 @@ export default {
   components: {wordsCarousel},
   data: function () {
     return {
+      predictionsArr: [],
       model: "",
       canvas: "",
       webcam: new tmImage.Webcam(400, 400, true),
@@ -108,25 +133,35 @@ export default {
       URL10: "https://teachablemachine.withgoogle.com/models/DTVQSSp5L/",
       URL5: "https://teachablemachine.withgoogle.com/models/USMunKY-N/",
       predictStop: false,
-      soundPlaying: false,
       onlyFiveWords: false,
+      active: '',
+      audioHashMap: {
+        מצויין: "metzuian",
+        שלום: "shalom",
+        היום: "hayom",
+        ברור: "barur",
+        מי: "mi",
+        מצטער: "mitztaer",
+        גבר: "gever",
+        אשה: "isha",
+        רגע: "rega",
+        אסור: "asur",
+      }
     }
   },
 
   methods: {
-    async playAudio(audioName) {
+    playAudio(audioName) {
       const audioObj = document.querySelector(`#${audioName}`)
       audioObj.play()
-      this.soundPlaying = true;
-      audioObj.onended = () => {
-        this.soundPlaying = false;
-        console.log("ended")
-      }
+      this.stop();
+      audioObj.onended = () => {}
     },
     // More API functions here:
     // https://github.com/googlecreativelab/teachablemachine-community/tree/master/libraries/image
     // Load the image model and setup the webcam
     async init() {
+      this.active = 'play';
       await this.webcam.setup();
       document.getElementById("webcam-container").appendChild(this.webcam.canvas);
       this.URL = this.onlyFiveWords ? this.URL5 : this.URL10;
@@ -141,7 +176,7 @@ export default {
     },
     async loop() {
       this.webcam.update(); // update the webcam frame
-      if (!this.predictStop && !this.soundPlaying) {
+      if (!this.predictStop) {
         await this.predict();
       }
       window.requestAnimationFrame(this.loop);
@@ -150,14 +185,49 @@ export default {
     async predict() {
       // predict can take in an image, video or canvas html element
       this.predictions = await this.model.predict(this.webcam.canvas);
-      this.prediction = this.predictions.find(prediction => prediction.probability > 0.8);
-     /* this.playAudio('metzuian');*/
+      this.prediction = await this.predictions.find(prediction => prediction.probability > 0.8);
+      if (this.prediction) {
+        this.predictionsArr.push(this.prediction.className)
+      }
+      if (this.predictionsArr.length > 20) {
+        this.prediction.className = this.mode(this.predictionsArr);
+        this.stop()
+        this.playAudio(this.audioHashMap[this.prediction.className]);
+        this.predictionsArr=[];
+      }
+
     },
     stop() {
       this.webcam.pause();
       this.predictStop = true;
+      this.active = 'stop';
+    },
+    play() {
+      this.webcam.play();
+      this.predictStop = false
+      this.active = 'play';
+    },
+    mode(array) {
+      if (array.length == 0)
+        return null;
+      let modeMap = {};
+      let maxEl = array[0], maxCount = 1;
+      for (let i = 0; i < array.length; i++) {
+        let el = array[i];
+        if (modeMap[el] == null)
+          modeMap[el] = 1;
+        else
+          modeMap[el]++;
+        if (modeMap[el] > maxCount) {
+          maxEl = el;
+          maxCount = modeMap[el];
+        }
+      }
+      return maxEl;
     }
   },
+
+
 }
 </script>
 
@@ -200,16 +270,23 @@ h4 {
   margin: 10px;
   padding: 1px;
 }
-.font_size{
+
+.font_size {
   font-weight: bold;
   font-size: 25px;
   color: #01579b;
   text-align: center;
 }
-.font_size2{
+
+.font_size2 {
   font-weight: bold;
   font-size: 25px;
   color: #e80202;
   text-align: center;
+}
+
+.active {
+  background-color: #C10015;
+  font-weight: bold;
 }
 </style>
